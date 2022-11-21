307ad083b229b4d0d280d85d0a0ed5db15e14b2b
Pixixixi <pixelf83@gmail.com>
2022-11-21 12:09:44 +0500

    v2

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,13 +1,19 @@
 package com.example.photogallery
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.photogallery.api.FlickrApi
@@ -18,27 +24,47 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
-    override fun onCreate(savedInstanceState:
-                          Bundle?) {
+    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchPhotos()
-        flickrLiveData.observe(
-            this,
-            Observer { responseString ->
-                Log.d(TAG, "Response received:$responseString")
-            })
-
+        photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+        retainInstance = true
+        thumbnailDownloader = ThumbnailDownloader()
+        lifecycle.addObserver(thumbnailDownloader)
     }
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
         photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
         return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photoGalleryViewModel.galleryItemLiveData.observe(this, Observer { galleryItems ->
+            photoRecyclerView.adapter = PhotoAdapter(galleryItems)
+            })
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycle.removeObserver(thumbnailDownloader)
+    }
+    private class PhotoHolder(private val itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView) {
+        val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+    }
+    private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>): RecyclerView.Adapter<PhotoHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_gallery, parent, false) as ImageView
+            return PhotoHolder(view)
+        }
+        override fun getItemCount(): Int = galleryItems.size
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            val placeholder: Drawable = ContextCompat.getDrawable(requireContext(), R.drawable.bill_up_close) ?: ColorDrawable()
+            holder.bindDrawable(placeholder)
+            thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
+        }
     }
     companion object {
         fun newInstance() = PhotoGalleryFragment()
--- app/src/main/java/com/example/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -1,7 +1,9 @@
 package com.example.photogallery.api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
     @GET(
@@ -11,5 +13,7 @@ interface FlickrApi {
                 "&nojsoncallback=1" +
                 "&extras=url_s"
     )
-    fun fetchPhotos(): Call<String>
+    fun fetchPhotos(): Call<FlickrResponse>
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
